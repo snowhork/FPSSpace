632b17b357bb3726750632459cbee2bca889a5fa
kyoh <stayheresilence@gmail.com>
2017-03-18 11:28:50 +0900

    add cameraHeightManager

---
+++ Assets/Scripts/Players/CameraHeightManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace Players
+{
+    [RequireComponent(typeof(RigidbodyFirstPersonController))]
+    public class CameraHeightManager : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private float _heightWithStaning;
+        [SerializeField] private float _heightWithSquatting;
+        private RigidbodyFirstPersonController _firstPersonController;
+
+        private void Start()
+        {
+            _firstPersonController = GetComponent<RigidbodyFirstPersonController>();
+        }
+
+        private void Update()
+        {
+            var positionY = _firstPersonController.Squatting ? _heightWithSquatting : _heightWithStaning;
+            var pos = _camera.transform.position;
+            _camera.transform.position = new Vector3(pos.x, positionY, pos.z);
+        }
+    }
+}
